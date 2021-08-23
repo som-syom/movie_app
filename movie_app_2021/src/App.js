5685be0778101b94501c6664a05a7197f09c3b04
som-syom <dhyeon@student.42seoul.kr>
2021-08-23 21:05:09 +0900

    Feat - 4.1강 까지 title 만 가져와서  정렬

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -3,23 +3,39 @@ import axios from "axios";
 import Movie from "./Movie";
 
 class App extends React.Component {
-	state = {
-		isLoading: true,
-		movie: []
-	};
-	getMovies = async () => {
-		const {data :{data: { movies }}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");		
-		console.log(movies);
-	};
+		state = {
+			isLoading: true,
+			movies: []
+		};
+		getMovies = async () => {
+			const {
+				data: {
+					data: {
+						movies
+					}
+				}
+			} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+			this.setState({ movies, isLoading: false });
+		};
+	
 	componentDidMount() {
 		this.getMovies();
 	}
 	
 	render() {
-		const {isLoading} = this.state;
+		const {isLoading, movies } = this.state;
 		return (
 			<div>
-				{isLoading? "Loading...": "We are ready"}
+				{isLoading? "Loading...": movies.map(movie => {
+					return (
+							<Movie 
+							key={movie.id}
+							id={movie.id}
+							title={movie.title}
+							summary={movie.summary}
+							poster={movie.medium_cover_image} />
+					)
+				})}
 			</div>
 		);
 	}
--- movie_app_2021/src/Movie.js
+++ movie_app_2021/src/Movie.js
@@ -1,10 +1,17 @@
 import React from "react";
 import propType from "prop-types"
 
-function Movie() {
+function Movie({id, title, summary, poster}) {
 	return (
-		<h4>test</h4>
+		<h4>{title}</h4>
 	);
+}
+
+Movie.propType = {
+	id: propType.number.isRequired,
+	title: propType.string.isRequired,
+	summary: propType.string.isRequired,
+	poster: propType.string.isRequired
 }
 
 export default Movie;
